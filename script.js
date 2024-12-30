8bf3b5545ceef45ab1d1de3b2e53f0271096df4b
Raubizar <103142742+Raubizar@users.noreply.github.com>
2024-12-30 22:43:13 +0000

    re-design

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function submitEmail() {
-    var email = document.getElementById("email").value;
-    if (email) {
-        alert("Thank you for signing up! You will be updated with the latest BIM news and resources.");
-        document.getElementById("signup-form").reset();
-    } else {
-        alert("Please enter a valid email address.");
-    }
+function subscribeNewsletter() {
+    alert("Thank you for subscribing to our newsletter!");
 }
